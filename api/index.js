f53363cff3dacdb5bc4c3292d634101de0d02c57
Shruti Shah <shruti@gvmtechnologies.com>
2025-11-18 13:21:04 +0530

    Remove Node.js runtime configuration from API index file

--- api/index.js
+++ api/index.js
@@ -1,10 +1,6 @@
 // Vercel serverless function wrapper for Express app
 import app from "../server/index.js";
 
-export const config = {
-  runtime: "nodejs20.x",
-};
-
 export default function handler(req, res) {
   return app(req, res);
 }
